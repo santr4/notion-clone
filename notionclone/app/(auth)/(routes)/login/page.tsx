304c6e5f4258290344e39e574b5f9c6b834d697c
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-02-18 11:36:15 +0530

    folder structures and routing in nextjs

---
+++ notionclone/app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>This is a login page.</div>;
+};
+
+export default LoginPage;
---
+++ notionclone/app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,5 @@
+const RegisterPage = () => {
+  return <div>Login Page!</div>;
+};
+
+export default RegisterPage;
